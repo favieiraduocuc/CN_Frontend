648d645f98df523d16dd130c055ea9ab6dc01f95
Daniel Santander <daniel.santander@cajalosandes.cl>
2026-01-22 20:36:22 -0300

    feat: landig page + login with microsoft msal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { InteractionStatus } from '@azure/msal-browser';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ag-web-cloud-native';
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly _destroying$ = new Subject<void>();
+
+  private authService = inject(MsalService);
+  private msalBroadcastService = inject(MsalBroadcastService);
+
+  ngOnInit(): void {
+    // initialize MSAL
+    this.authService.instance.initialize().then(() => {
+      // handle redirects after login
+      this.authService.instance
+        .handleRedirectPromise()
+        .then((response) => {
+          if (response) {
+            console.log('Login successful:', response);
+          }
+        })
+        .catch((error) => {
+          console.error('Error in redirect:', error);
+        });
+    });
+
+    // subscribe to changes in interaction state
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter(
+          (status: InteractionStatus) => status === InteractionStatus.None,
+        ),
+        takeUntil(this._destroying$),
+      )
+      .subscribe(() => {
+        // do something after interaction ends
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
 }
---
+++ src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,105 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import {
+  AccountInfo,
+  EventMessage,
+  EventType,
+  InteractionStatus,
+} from '@azure/msal-browser';
+import { filter, Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css',
+})
+export class LandingPageComponent implements OnInit, OnDestroy {
+  private readonly _destroying$ = new Subject<void>();
+  isLoggedIn = false;
+  userAccount: AccountInfo | null = null;
+
+  private authService = inject(MsalService);
+  private msalBroadcastService = inject(MsalBroadcastService);
+  private router = inject(Router);
+
+  capabilities = [
+    {
+      icon: '📍',
+      title: 'Rastreo en tiempo real',
+      description:
+        'Rastreo en tiempo real de vehículos con GPS, asegurando que siempre sepas dónde están tus activos.',
+    },
+    {
+      icon: '🕒',
+      title: 'Programación Inteligente',
+      description:
+        'Optimización de rutas y reducción de retrasos con planificación inteligente.',
+    },
+    {
+      icon: '📊',
+      title: 'Análisis de Datos',
+      description:
+        'Análisis de datos para obtener insights prácticos sobre el flujo de pasajeros y eficiencia para tomar decisiones operativas informadas.',
+    },
+  ];
+
+  loginWithMicrosoft() {
+    console.log('login with Microsoft...');
+    this.authService.loginRedirect({
+      scopes: ['openid', 'offline_access'],
+    });
+  }
+
+  checkLoginStatus(): void {
+    const accounts = this.authService.instance.getAllAccounts();
+    this.isLoggedIn = accounts.length > 0;
+    if (this.isLoggedIn) {
+      this.userAccount = accounts[0];
+      this.authService.instance.setActiveAccount(this.userAccount);
+      console.log(
+        'User authenticated:',
+        this.userAccount?.name || this.userAccount?.username,
+      );
+    } else {
+      console.log('No user authenticated');
+    }
+  }
+
+  ngOnInit(): void {
+    this.checkLoginStatus();
+
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter(
+          (msg: EventMessage) =>
+            msg.eventType === EventType.LOGIN_SUCCESS ||
+            msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS,
+        ),
+        takeUntil(this._destroying$),
+      )
+      .subscribe((result: EventMessage) => {
+        console.log('detected auth event:', result.eventType);
+        this.checkLoginStatus();
+      });
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter(
+          (status: InteractionStatus) => status === InteractionStatus.None,
+        ),
+        takeUntil(this._destroying$),
+      )
+      .subscribe(() => {
+        this.checkLoginStatus();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+}
